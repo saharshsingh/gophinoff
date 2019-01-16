3c7f41cff377f24762b8fac442a3849967a070a0
Saharsh Singh <sahsingh@redhat.com>
2019-01-16 12:15:26 -0500

    Added support to build container using Buildah. Improved cleanup management in bash script

--- taskmaster/build.sh
+++ taskmaster/build.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 
+cleanup() {
+
+    # Remove working container from buildah
+    if [ "$tm_container" != "" ]; then buildah rm $tm_container; fi
+
+    # Go back to user directory
+    popd
+}
+
 # Define script constants
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Test code
+# Process CLI args
+USE_BUILDAH=0
+if [ "$1" == "--using-buildah" ]; then
+    USE_BUILDAH=1
+fi
+
+# Change directory to code location
+set -e
 pushd $SCRIPT_DIR
-set +e
+trap cleanup EXIT INT TERM
+
+# Test code
 go test ./...
 
-# Exit if tests failed
-TESTS_PASSED=$?
-popd
-if [ $TESTS_PASSED -ne 0 ]; then exit $TESTS_PASSED; fi
+# Build Go binary
+time CGO_ENABLED=0 go build
 
-# Build
-set -e
-time CGO_ENABLED=0 go build -a $SCRIPT_DIR
-docker build -t saharsh/taskmaster $SCRIPT_DIR
+# Build container
+if [ $USE_BUILDAH -eq 1 ]; then
+    tm_container=$(buildah from scratch)
+    buildah copy $tm_container taskmaster /
+    buildah config --port 8080 --entrypoint '["/taskmaster"]' --label "maintainer=Saharsh Singh" $tm_container
+    buildah commit $tm_container saharsh/taskmaster
+else
+    docker build -t saharsh/taskmaster $SCRIPT_DIR
+fi
